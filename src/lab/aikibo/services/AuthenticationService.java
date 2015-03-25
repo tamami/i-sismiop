43aa3e8823be31f7be698219d6d4cb411df4942e
tamami <tamami@tamami-pc>
2015-03-25 15:25:15 +0700

    tambah loginDummy

--- src/lab/aikibo/services/AuthenticationService.java
+++ src/lab/aikibo/services/AuthenticationService.java
@@ -2,8 +2,11 @@ package lab.aikibo.services;
 
 public interface AuthenticationService {
 	
+	/** login with any account -- just for init app */
+	public boolean loginDummy(String nmLogin, String password);
+	
 	/** login with account and password */
-	public boolean login(String account, String password);
+	public boolean login(String nmLogin, String password);
 	
 	/** logout current user */
 	public void logout();
