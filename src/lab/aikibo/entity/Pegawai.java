298655dcb7ae91aad9c4c5d50bbff62a1171e81d
tamami <tamami@tamami-pc>
2015-03-25 15:11:42 +0700

    init commit

---
+++ src/lab/aikibo/entity/Pegawai.java
@@ -0,0 +1,33 @@
+package lab.aikibo.entity;
+
+public class Pegawai {
+	
+	private String nip;
+	private String nmPegawai;
+	private String nipBaru;
+	
+	public String getNip() {
+		return nip;
+	}
+	
+	public void setNip(String nip) {
+		this.nip = nip;
+	}
+
+	public String getNmPegawai() {
+		return nmPegawai;
+	}
+
+	public void setNmPegawai(String nmPegawai) {
+		this.nmPegawai = nmPegawai;
+	}
+
+	public String getNipBaru() {
+		return nipBaru;
+	}
+
+	public void setNipBaru(String nipBaru) {
+		this.nipBaru = nipBaru;
+	}
+
+}
