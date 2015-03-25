c591f6e78cf2b0f6c9cdf9b440115f1c3304673c
tamami <tamami@tamami-pc>
2015-03-25 15:08:55 +0700

    init commit

---
+++ src/lab/aikibo/constant/StatusEntry.java
@@ -0,0 +1,9 @@
+package lab.aikibo.constant;
+
+public class StatusEntry {
+	
+	public final static int DATA_BARU = 1;
+	public final static int UBAH_DATA = 2;
+	public final static int HAPUS_DATA = 3;
+
+}
