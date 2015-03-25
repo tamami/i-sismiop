911d2a6c1ad1a9e22146b4dfedc6c267ccac87df
tamami <tamami@tamami-pc>
2015-03-25 15:39:21 +0700

    update from github

--- src/lab/aikibo/viewmodel/GlobalController.java
+++ src/lab/aikibo/viewmodel/GlobalController.java
@@ -26,4 +26,10 @@ public class GlobalController extends SelectorComposer<Component>
 		Clients.showNotification("login.zul sudah dipanggil");
 	}
 	
+	@GlobalCommand
+	public void callAdmUser() {
+		Include include = (Include) Selectors.iterable(getPage(), "#mainInclude").iterator().next();
+		include.setSrc("/form/adm_user.zul");
+	}
+	
 }
