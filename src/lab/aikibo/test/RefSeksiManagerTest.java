dd8adb2b209dd492d12f5e60b403ed2951182712
tamami <tamami@tamami-pc>
2015-04-27 08:47:44 +0700

    init commit

---
+++ src/lab/aikibo/test/RefSeksiManagerTest.java
@@ -0,0 +1,12 @@
+package lab.aikibo.test;
+
+import lab.aikibo.manager.RefSeksiManager;
+
+public class RefSeksiManagerTest {
+	
+	public static void main(String args[]) {
+		RefSeksiManager rsm = new RefSeksiManager();
+		System.out.println(rsm.getNamaSeksiByKode("35"));
+	}
+
+}
