8318e4c0fccbd971e5510cb59013088b3782fd4e
tamami <tamami@tamami-pc>
2015-04-27 08:53:44 +0700

    init commit

---
+++ src/lab/aikibo/entity/RefSeksi.java
@@ -0,0 +1,50 @@
+package lab.aikibo.entity;
+
+public class RefSeksi {
+	private String kdSeksi;
+	private String nmSeksi;
+	private String noSrtSeksi;
+	private String kodeSurat1;
+	private String kodeSurat2;
+	
+	public String getKdSeksi() {
+		return kdSeksi;
+	}
+	
+	public void setKdSeksi(String kdSeksi) {
+		this.kdSeksi = kdSeksi;
+	}
+
+	public String getNmSeksi() {
+		return nmSeksi;
+	}
+
+	public void setNmSeksi(String nmSeksi) {
+		this.nmSeksi = nmSeksi;
+	}
+
+	public String getNoSrtSeksi() {
+		return noSrtSeksi;
+	}
+
+	public void setNoSrtSeksi(String noSrtSeksi) {
+		this.noSrtSeksi = noSrtSeksi;
+	}
+
+	public String getKodeSurat1() {
+		return kodeSurat1;
+	}
+
+	public void setKodeSurat1(String kodeSurat1) {
+		this.kodeSurat1 = kodeSurat1;
+	}
+
+	public String getKodeSurat2() {
+		return kodeSurat2;
+	}
+
+	public void setKodeSurat2(String kodeSurat2) {
+		this.kodeSurat2 = kodeSurat2;
+	}
+	
+}
