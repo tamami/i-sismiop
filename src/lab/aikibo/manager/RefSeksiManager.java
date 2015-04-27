1d46d6ac0d4595764f497a66ab1c86fea7430f8d
tamami <tamami@tamami-pc>
2015-04-27 08:52:55 +0700

    init commit

---
+++ src/lab/aikibo/manager/RefSeksiManager.java
@@ -0,0 +1,35 @@
+package lab.aikibo.manager;
+
+import java.util.List;
+
+import lab.aikibo.entity.RefSeksi;
+
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.criterion.Restrictions;
+import org.zkoss.zkplus.hibernate.HibernateUtil;
+
+public class RefSeksiManager {
+	
+	public String getNamaSeksiByKode(String kode) {
+		Session session = HibernateUtil.currentSession();
+		String result = null;
+		
+		session.beginTransaction();
+		Criteria criteria = session.createCriteria(RefSeksi.class);
+		criteria.add(Restrictions.eq("kdSeksi", kode));
+		List<RefSeksi> data = (List<RefSeksi>)criteria.list();
+		if(data.size() > 0) {
+			result = data.get(0).getNmSeksi();
+		}
+		return result;
+	}
+	
+	public List<RefSeksi> getDaftarSeksi() {
+		Session session = HibernateUtil.currentSession();
+		session.beginTransaction();
+		List<RefSeksi> data = (List<RefSeksi>) session.createQuery("from RefSeksi").list();
+		return data;
+	}
+
+}
