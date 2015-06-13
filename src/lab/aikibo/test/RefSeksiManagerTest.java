33dd599c135c6854b37519aea44d5757f9cdfa37
tamami <tamami@tamami-pc>
2015-06-13 15:36:41 +0700

    unit test untuk Management tabel ref_seksi

--- src/lab/aikibo/test/RefSeksiManagerTest.java
+++ src/lab/aikibo/test/RefSeksiManagerTest.java
@@ -1,12 +1,30 @@
 package lab.aikibo.test;
 
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import lab.aikibo.entity.RefSeksi;
 import lab.aikibo.manager.RefSeksiManager;
 
 public class RefSeksiManagerTest {
 	
-	public static void main(String args[]) {
-		RefSeksiManager rsm = new RefSeksiManager();
-		System.out.println(rsm.getNamaSeksiByKode("35"));
+	private RefSeksiManager rsm;
+	
+	@Before
+	public void init() {
+		rsm = new RefSeksiManager();
+	}
+	
+	@Test
+	public void testGetDataByKode() {
+		RefSeksi rs = rsm.getDataSeksiByKode("10");
+		assertEquals("10", rs.getKdSeksi());
+		assertEquals("SUB BAGIAN TATA USAHA", rs.getNmSeksi());
+		assertEquals("01", rs.getNoSrtSeksi());
+		assertEquals("WPJ", rs.getKodeSurat1());
+		assertEquals("KB", rs.getKodeSurat2());
 	}
 
 }
